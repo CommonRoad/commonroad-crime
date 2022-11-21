163446a7dad49652fec1a8853f0ac16648541543
yuanfei <yuanfei.lin@tum.de>
2022-11-21 11:34:42 +0100

    structure of STN

---
+++ commonroad_crime/metric/index_scale/stn.py
@@ -0,0 +1,41 @@
+__author__ = "Yuanfei Lin"
+__copyright__ = "TUM Cyber-Physical Systems Group"
+__credits__ = ["KoSi"]
+__version__ = "0.0.1"
+__maintainer__ = "Yuanfei Lin"
+__email__ = "commonroad@lists.lrz.de"
+__status__ = "Pre-alpha"
+
+import logging
+import matplotlib.pyplot as plt
+
+from commonroad_crime.data_structure.base import CriMeBase
+from commonroad_crime.data_structure.configuration import CriMeConfiguration
+from commonroad_crime.data_structure.type import TypeIndexScale
+from commonroad_crime.metric.distance_scale.hw import HW
+import commonroad_crime.utility.general as utils_gen
+import commonroad_crime.utility.logger as utils_log
+import commonroad_crime.utility.solver as utils_sol
+import commonroad_crime.utility.visualization as utils_vis
+
+logger = logging.getLogger(__name__)
+
+
+class STN(CriMeBase):
+    """
+    the ratio between the requested lateral acceleration (due to steering) and the maximum achievable lateral
+    acceleration.
+
+    -- from Hosseini S, Murgovski N, De Campos GR, Sjöberg J. Adaptive forward collision warning algorithm for
+    automotive applications. In2016 American Control Conference (ACC) 2016 Jul 6 (pp. 5982-5987). IEEE.
+    """
+    metric_name = TypeIndexScale.STN
+
+    def __init__(self, config: CriMeConfiguration):
+        super(STN, self).__init__(config)
+
+    def compute(self, vehicle_id: int, time_step: int = 0):
+        pass
+
+    def visualize(self):
+        pass
