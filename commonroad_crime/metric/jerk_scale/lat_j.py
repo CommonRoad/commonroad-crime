8b2eaaf9dfab7d1a9975d08c5bc1971f1a276c32
yuanfei <yuanfei.lin@tum.de>
2022-11-22 14:57:39 +0100

    add check for initial state and remove the redundant part from jerk

--- commonroad_crime/metric/jerk_scale/lat_j.py
+++ commonroad_crime/metric/jerk_scale/lat_j.py
@@ -30,53 +30,11 @@ class LatJ(CriMeBase):
     def __init__(self, config: CriMeConfiguration):
         super(LatJ, self).__init__(config)
 
-    def _compute_jerk(self, evaluated_state: State):
-        if hasattr(evaluated_state, 'jerk'):
-            return evaluated_state.jerk
-        else:
-            if hasattr(evaluated_state, 'acceleration'):
-                current_acc = evaluated_state.acceleration
-            elif self.ego_vehicle.state_at_time(self.time_step + 1):
-                current_acc = utils_sol.compute_acceleration(
-                    evaluated_state.velocity,
-                    self.ego_vehicle.state_at_time(self.time_step + 1).velocity,
-                    self.dt
-                )
-            else:
-                current_acc = None
-            if self.ego_vehicle.state_at_time(self.time_step + 1):
-                if hasattr(self.ego_vehicle.state_at_time(self.time_step + 1), 'acceleration'):
-                    next_acc = self.ego_vehicle.state_at_time(self.time_step + 1).acceleration
-                elif self.ego_vehicle.state_at_time(self.time_step + 2):
-                    next_acc = utils_sol.compute_acceleration(
-                        self.ego_vehicle.state_at_time(self.time_step + 1).velocity,
-                        self.ego_vehicle.state_at_time(self.time_step + 1).velocity,
-                        self.dt
-                    )
-                else:
-                    next_acc = None
-            else:
-                next_acc = None
-            if current_acc is not None and next_acc is not None:
-                return utils_sol.compute_jerk(
-                    current_acc,
-                    next_acc,
-                    self.dt)
-            else:
-                return None
-
     def compute(self, time_step: int):
         self.time_step = time_step
         utils_log.print_and_log_info(logger, f"* Computing the {self.metric_name} at time step {time_step}")
         evaluated_state = self.ego_vehicle.state_at_time(self.time_step)
-        jerk = self._compute_jerk(evaluated_state)
-        if jerk is not None:
-            self.value = utils_gen.int_round(jerk * math.sin(evaluated_state.orientation), 2)
-            utils_log.print_and_log_info(logger, f"*\t\t {self.metric_name} = {self.value}")
-        else:
-            self.value = None
-            utils_log.print_and_log_info(logger, f"*\t\t {self.metric_name} is none, but you can interpolate it using "
-                                                 f"the values from previous time steps")
+        self.value = utils_gen.int_round(evaluated_state.jerk * math.sin(evaluated_state.orientation), 2)
         return self.value
 
     def visualize(self):
