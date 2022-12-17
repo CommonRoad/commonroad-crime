31c26b6bbaee41a93fdcb09439c93d076e6eb091
yuanfei <yuanfei.lin@tum.de>
2022-12-17 16:33:49 +0100

    set up the config and basic structure

--- commonroad_crime/data_structure/configuration.py
+++ commonroad_crime/data_structure/configuration.py
@@ -93,6 +93,7 @@ class GeneralConfiguration:
 
         self.name_scenario = name_scenario
         self.path_scenarios = config_relevant.path_scenarios
+        self.path_scenarios_batch = config_relevant.path_scenarios_batch
         self.path_output = config_relevant.path_output + name_scenario + "/"
         self.path_logs = config_relevant.path_logs
         self.path_icons = config_relevant.path_icons
--- commonroad_crime/utility/batch_evaluation.py
+++ commonroad_crime/utility/batch_evaluation.py
@@ -6,13 +6,35 @@ __maintainer__ = "Yuanfei Lin"
 __email__ = "commonroad@lists.lrz.de"
 __status__ = "Pre-alpha"
 
+import multiprocessing
 import os
+from typing import List
+import logging
 import fnmatch
+from enum import Enum, unique
 from typing import Dict
 
 from commonroad_crime.data_structure.configuration import CriMeConfiguration
-
+import commonroad_crime.utility.logger as utils_log
 from commonroad.common.file_reader import CommonRoadFileReader
+
+logger = logging.getLogger(__name__)
+
+
+def initialize_process(config: CriMeConfiguration, flag_multi_processing: bool = True,):
+    scenario_loader = ScenarioLoader(config.general.path_scenarios_batch)
+    if flag_multi_processing:
+        manager = multiprocessing.Manager()
+        result_dict = manager.dict()
+    else:
+        result_dict = dict()
+    result_dict["Success"] = list()
+    result_dict["Failure"] = list()
+    result_dict["Criticality"] = list()
+    result_dict["scenarios_to_process"] = scenario_loader.scenario_ids
+    result_dict["num_scenarios"] = len(scenario_loader.scenario_ids)
+    result_dict["started_processing"] = 0
+    return result_dict
 
 
 def run_parallel(config: CriMeConfiguration):
@@ -36,12 +58,15 @@ def run_sequential():
 class ScenarioLoader:
     def __init__(self, scenario_folder: str,):
         self.scenario_folder = scenario_folder
+        self.scenario_ids = self._obtain_scenarios_with_path()
 
-    def load_scenarios(self, scenario_id: str, verbose: bool = False):
-        pass
-
-    def _obtain_scenarios_with_path(self):
-        scenarios: Dict[str, str] = dict()
+    def _obtain_scenarios_with_path(self) -> List:
+        scenario_ids = list()
+        extension_length = len('.xml')
         for path, directories, files in os.walk(self.scenario_folder):
             for scenario in fnmatch.filter(files, "*.xml"):
-                pass
+                scenario_ids.append(scenario[:-extension_length])
+        if len(scenario_ids) == 0:
+            utils_log.print_and_log_warning(logger, f"No Scenario found in directory: {self.scenario_folder}")
+        utils_log.print_and_log_info(logger, f"Number of scenarios:{len(scenario_ids)}")
+        return scenario_ids
