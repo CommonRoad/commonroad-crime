9571c0864bdc20b14142340d3e3a8926628d577b
yuanfei <yuanfei.lin@tum.de>
2022-12-30 09:18:18 +0100

    update evaluation script

--- tutorials/evaluate_ttb_example.py
+++ tutorials/evaluate_ttb_example.py
@@ -6,12 +6,12 @@ from commonroad_crime.metric.time_scale.ttb import TTB
 from commonroad_crime.metric.time_scale.ttr import TTR
 from commonroad_crime.metric.time_scale.wttr import WTTR
 from commonroad_crime.metric.time_scale.ttc_star import TTCStar
-
+from commonroad_crime.metric.time_scale.wttc import WTTC
 
 def main():
 
     scenario_id = 'ZAM_Urban-7_1_S-2'
-
+    other_id = 99
     # ==== build configuration
     config = ConfigurationBuilder.build_configuration(scenario_id)
     config.update()
@@ -27,6 +27,16 @@ def main():
     # #config.debug.save_plots = False
     # ttc_interface.visualize()
 
+    # ********** WTTC **********
+    # ==== select the criticality metric
+    wttc_interface = WTTC(config)
+    # # ==== evaluate at time step 0
+    wttc_interface.compute(other_id, 0)
+    #
+    # # ==== visualize the result
+    # config.debug.save_plots = False
+    wttc_interface.visualize()
+
     # ********** TTR **********
     # # ==== select the criticality metric
     # ttr_interface = TTR(config)
@@ -36,18 +46,18 @@ def main():
     # # ==== visualize the result
     # #config.debug.save_plots = False
     # ttr_interface.visualize()
-
-    # ************************
-    # ********** WTTR **********
-    # ==== select the criticality metric
-    wttr_interface = WTTR(config)
-    # # ==== evaluate at time step 0
-    wttr_interface.compute(0, verbose=True)
     #
-    # # ==== visualize the result
-    #config.debug.save_plots = False
-    wttr_interface.visualize()
-    # ************************
+    # # ************************
+    # #********** WTTR **********
+    # #==== select the criticality metric
+    # wttr_interface = WTTR(config)
+    # # # ==== evaluate at time step 0
+    # wttr_interface.compute(0, verbose=True)
+    # #
+    # # # ==== visualize the result
+    # #config.debug.save_plots = False
+    # wttr_interface.visualize()
+    #************************
 
 
 if __name__ == "__main__":
