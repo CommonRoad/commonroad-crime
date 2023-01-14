4631d0446dc074fe337ea4da776dd8e462ad3dc9
yuanfei <yuanfei.lin@tum.de>
2023-01-14 11:12:47 +0100

    add schematic scenario from openScenario

--- tutorials/evaluate_criticality.py
+++ tutorials/evaluate_criticality.py
@@ -17,8 +17,9 @@ from commonroad_crime.data_structure.crime_interface import CriMeInterface
 
 def main():
 
-    # scenario_id = 'ZAM_Urban-7_1_S-2'
-    scenario_id = "DEU_Gar-1_1_T-1"
+    scenario_id = 'ZAM_Urban-7_1_S-2'
+    #scenario_id = "DEU_Gar-1_1_T-1"
+    scenario_id = "OSC_CutIn-1_2_T-1"
 
     # ==== build configuration
     config = ConfigurationBuilder.build_configuration(scenario_id)
@@ -27,21 +28,29 @@ def main():
 
     # ==== compute the criticality using CriMe interface
     crime_interface = CriMeInterface(config)
-    # crime_interface.evaluate([THW])
+    crime_interface.evaluate_scenario([THW], 0, 50)
     # crime_interface.evaluate([HW, THW, TTC, WTTC, TTCStar, TTS, TTK, TTB, TTR, ])
-    # crime_interface.evaluate([WTTR, DA])
+    # crime_interface.evaluate_scene([WTTR])
+    # WTTR_obj = WTTR(config)
+    # WTTR_obj.compute(0)
+    # WTTR_obj.visualize()
     # crime_interface.evaluate_scene([ALatReq, ALongReq, LongJ, LatJ, BTN, STN])
-    crime_interface.evaluate_scenario([ALatReq, ALongReq, LongJ, LatJ, BTN, STN], 0, 20)
-
+    # crime_interface.evaluate_scenario([TTC, DA, ALongReq, BTN, ALatReq, STN, P_MC, PF], 0, 20)
+    # crime_interface.evaluate_scenario([ALatReq], 0, 20)
+    #
+    # crime_interface.evaluate_scenario([PF], 0, 20)
     # crime_interface.evaluate_scene([PF], 12)
     # crime_interface.evaluate_scene([TTC], 12, vehicle_id=202)
-    # crime_interface.evaluate([P_MC, PF])
+    #crime_interface.evaluate_scene([BTN], 0)
     # crime_interface.evaluate_scenario([DeltaV], 0, 30)
     # ==== select the criticality metric you want to evaluate and then compute the value at a given time step
-
+    # da = DA(config)
+    utils_vis.visualize_scenario_at_time_steps(crime_interface.config.scenario,
+                                               plot_limit=[80, 170, -5, 5], time_steps=[20, 34, 99])
     # # ==== visualize the result
     # config.debug.save_plots = False
     utils_vis.plot_criticality_curve(crime_interface)
+
 
 if __name__ == "__main__":
     main()
