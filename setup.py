418a5b56b508cbbe3d501a9c4e780fe5c066c4f3
yuanfei <yuanfei.lin@tum.de>
2023-02-25 03:55:21 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(name='commonroad-crime',
           'commonroad-drivability-checker==2021.4',
           'commonroad-vehicle-models>=1.0.0'
           'matplotlib>=3.5.2'
-          'numpy>=1.19.5',
-          'scipy==1.7.3',
+          'numpy>=1.23.0',
+          'scipy==1.10.1',
           'shapely==1.7.1',
           'omegaconf>=2.1.1',
           'casadi>=3.5.5',
