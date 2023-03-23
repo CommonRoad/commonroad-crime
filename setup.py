2d014c389e2829638fb7f9b9471a4e095c2ef38b
yuanfei <yuanfei.lin@tum.de>
2023-03-23 16:01:21 +0100

    version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='commonroad-crime',
-      version='0.1.0',
+      version='0.1.1',
       description='criticality measures of automated vehicles',
       keywords="criticality, autonomous driving",
       author='yuanfei Lin',
