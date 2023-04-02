55c36e3caef75a9aa9ddca8bce8f61f1f6b9b760
yuanfei <yuanfei.lin@tum.de>
2023-04-02 13:47:13 +0200

    TET inherent from TIT

--- commonroad_crime/measure/time/tet.py
+++ commonroad_crime/measure/time/tet.py
@@ -6,20 +6,29 @@ __maintainer__ = "Yuanfei Lin"
 __email__ = "commonroad@lists.lrz.de"
 __status__ = "Pre-alpha"
 
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+import numpy as np
+import logging
+
 from commonroad_crime.data_structure.base import CriMeBase
 from commonroad_crime.data_structure.configuration import CriMeConfiguration
 from commonroad_crime.data_structure.type import TypeTime, TypeMonotone
+from commonroad_crime.measure.time.tit import TIT
+import commonroad_crime.utility.visualization as utils_vis
+from commonroad_crime.utility.visualization import TUMcolor
+import commonroad_crime.utility.logger as utils_log
+import commonroad_crime.utility.general as utils_gen
 
-from commonroad_crime.measure.time.ttc import TTC
+logger = logging.getLogger(__name__)
 
 
-class TET(CriMeBase):
+class TET(TIT):
     measure_name = TypeTime.TET
     monotone = TypeMonotone.POS
 
     def __init__(self, config: CriMeConfiguration):
         super(TET, self).__init__(config)
-        self.ttc_object = TTC(config)
 
     def compute(self, vehicle_id: int, time_step: int = 0):
         """
@@ -33,9 +42,10 @@ class TET(CriMeBase):
 
         self.value = 0
         for i in range(time_step, len(state_list)):
-            if self.ttc_object.compute(vehicle_id, i) <= tau:
+            ttc_result = self.ttc_object.compute(vehicle_id, i)
+            self._ttc_cache[i] = ttc_result
+            if ttc_result <= tau:
                 self.value += self.dt
+        self.value = utils_gen.int_round(self.value, 4)
+        utils_log.print_and_log_info(logger, f"*\t\t {self.measure_name} = {self.value}")
         return self.value
-
-    def visualize(self):
-        pass
