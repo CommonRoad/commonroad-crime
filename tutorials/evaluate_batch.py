574659d587b5f2ea6336339076edef10da3e940d
yuanfei <yuanfei.lin@tum.de>
2023-04-04 17:56:53 +0200

    fix the None types and batch script

--- tutorials/evaluate_batch.py
+++ tutorials/evaluate_batch.py
@@ -1,13 +1,10 @@
-
-import os
-import sys
 import commonroad_crime.utility.batch_evaluation as utils_batch
 from commonroad_crime.measure import TTC
 
 
 def main():
-    scenario_path = "/home/yuanfei/commonroad/commonroad-criticality-measures/scenarios/"
-    config_root = "/home/yuanfei/commonroad/commonroad-criticality-measures/"
+    scenario_path = " "
+    config_root = " "
     utils_batch.run_sequential(scenario_path=scenario_path,
                                config_root=config_root,
                                measures=[TTC])
