d5d90d3ed29f6cffbec8123862330b1e52d72fa7
yuanfei <yuanfei.lin@tum.de>
2023-04-19 17:18:47 +0200

    fix TCI

--- commonroad_crime/data_structure/base.py
+++ commonroad_crime/data_structure/base.py
@@ -62,7 +62,7 @@ class CriMeBase:
                                                                            f'{self.configuration.vehicle.ego_id} is ' \
                                                                            'not contained in the scenario>'
         # =======       Vehicles      ========
-        self.ego_vehicle: DynamicObstacle = copy.deepcopy(self.sce.obstacle_by_id(self.configuration.vehicle.ego_id))
+        self.ego_vehicle: DynamicObstacle = self.sce.obstacle_by_id(self.configuration.vehicle.ego_id)
         if not isinstance(self.ego_vehicle, StaticObstacle) and \
                 not isinstance(self.ego_vehicle.prediction, SetBasedPrediction):
             utils_gen.check_elements_state_list([self.ego_vehicle.initial_state] + self.ego_vehicle.
