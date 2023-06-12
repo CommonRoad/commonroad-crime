81160190b413d50033d1628601aa6f4ec02a1fd4
Kun Qian <ge92bah@mytum.de>
2023-06-12 23:45:43 +0200

    Sorry, forgot to upload it.

--- commonroad_crime/measure/index/ci.py
+++ commonroad_crime/measure/index/ci.py
@@ -44,11 +44,8 @@ class CI(CriMeBase):
     def compute(self, vehicle_id: int, time_step: int = 0, verbose: bool = True):
         utils_log.print_and_log_info(logger, f"* Computing the {self.measure_name} at time step {time_step}")
         self.set_other_vehicles(vehicle_id)
-        self.time_step = time_step
         state_list_ego = self.ego_vehicle.prediction.trajectory.state_list
         state_list_other = self.other_vehicle.prediction.trajectory.state_list
-        
-        
         
         # Only for scenarios with intersection tag
         if Tag.INTERSECTION not in self.sce.tags:
@@ -56,12 +53,10 @@ class CI(CriMeBase):
             self.value = 0
             return self.value
         
-
-
         # In paper, assumption that released collision energy affects vehicle occupants
         alpha = 1.0
 
-        #Since the conflict index is only estimated for one type of conflict, β is assumed to be 1.0. (paper)
+        # Since the conflict index is only estimated for one type of conflict, β is assumed to be 1.0. (paper)
         # beta depends on conflict type and surrounding collisions
         beta = 1.0
 
@@ -69,23 +64,31 @@ class CI(CriMeBase):
         m1 = 1500
         m2 = 1500
 
-        # Use in case of existing PET
-        # pet = self._pet_object.compute(vehicle_id, time_step)
-        pet = 2.0
-
+        # Use PET object
+        pet = self._pet_object.compute(vehicle_id, time_step)
+        print("PET")
+        print(pet)
         if pet >= 5:
-            self.value = 0
+            value = 0
+            return value
         else:
-            coll_time_ego = -1
-            coll_time_other = -1
 
             # Find whether a conflict area exists first: Check whether the two vehicles overlap at a certain point in time
+            
+
+            intersection = None
+            coll_time_ego = -1
+            coll_time_other = -1
             for i in range(time_step, len(state_list_ego)):
+                
                 ego_poly=self.ego_vehicle.occupancy_at_time(i).shape.shapely_object
                 for j in range(time_step, len(state_list_other)):
                     if self.other_vehicle.occupancy_at_time(j) is not None:
+                        #print("OOOO")
                         other_poly = self.other_vehicle.occupancy_at_time(j).shape.shapely_object
-                        if ego_poly.crosses(other_poly):
+                        #print(other_poly)
+                        if ego_poly.intersects(other_poly):
+                            self.intersection = ego_poly.intersection(other_poly)
                             coll_time_ego = i
                             coll_time_other = j
                             break
@@ -97,128 +100,69 @@ class CI(CriMeBase):
             # Without trajectory collision, CI is zero
             if coll_time_ego == -1:
                 utils_log.print_and_log_info(logger, f"* No trajectory intersection found between ego vehicle and vehicle id {vehicle_id}")
-                self.value = 0
-                return self.value
-
-
-            # We use lanelet orientation for current vehicle orientation
-            
-            ego_lanelet_id = self.sce.lanelet_network.find_lanelet_by_position([self.ego_vehicle.state_at_time(coll_time_ego).
-                                                                       position])[0]
-            other_lanelet_id = self.sce.lanelet_network.find_lanelet_by_position([self.other_vehicle.state_at_time(coll_time_other).
-                                                                       position])[0]
-
-            
-            ego_orientation = utils_sol.compute_lanelet_width_orientation(
-                self.sce.lanelet_network.find_lanelet_by_id(ego_lanelet_id[0]),
-                self.ego_vehicle.state_at_time(time_step).position
-            )[1]
-            
-            other_orientation = utils_sol.compute_lanelet_width_orientation(
-                self.sce.lanelet_network.find_lanelet_by_id(other_lanelet_id[0]),
-                self.other_vehicle.state_at_time(time_step).position
-            )[1]
-            theta1 = ego_orientation
-            theta2 = other_orientation
+                value = 0
+                return value
             
             state_ego = self.ego_vehicle.state_at_time(coll_time_ego)
             state_other = self.other_vehicle.state_at_time(coll_time_other)
             
             
             
-            v_x = (m1 * state_ego.velocity + m2 * state_other.velocity)/(m1+m2)
-            v_y = (m1 * state_ego.velocity_y + m2 * state_other.velocity_y)/(m1+m2)
-            v = math.sqrt(v_x**2 +v_y**2)
-            
-            ###ORIGINAL
-            """
+            self.v_x = (m1 * state_ego.velocity + m2 * state_other.velocity)/(m1+m2)
+            self.v_y = (m1 * state_ego.velocity_y + m2 * state_other.velocity_y)/(m1+m2)
+            v = math.sqrt(self.v_x**2 +self.v_y**2)
             
             u1 = math.sqrt(state_ego.velocity ** 2 + state_ego.velocity_y ** 2)
             u2 = math.sqrt(state_other.velocity ** 2 + state_other.velocity_y ** 2)
 
-            # Fix bug for theta1=theta2=0
-            var1 = (m1 * u1 * math.sin(theta1) + m2 * u2 * math.sin(theta2))
-            var2 = (m1 * u1 * math.cos(theta1) + m2 * u2 * math.cos(theta2))
-            denom =    (m1+m2) * math.sin(math.atan2(var1, var2 ) )
-            epsilon = float(denom == 0) * 1e-10
-            
-            v = var1    / (denom + epsilon  )
-            """
             k_delta = 1/2*(m1 * u1**2) + 1/2*(m2 * u2**2) - 1/2*(m1+m2) * v**2
-
-
             self.value = alpha * k_delta / math.exp(beta * pet)
+
+            # Save parameters for visualization
+            self.cte = coll_time_ego
+            self.cto = coll_time_other
+
             return self.value
 
-    def visualize(self): #, figsize: tuple = (25, 15)):
-        self._initialize_vis()#figsize=figsize,
-                             #plot_limit=utils_vis.
-                             #plot_limits_from_state_list(self.time_step,
-                             #self.ego_vehicle.prediction.trajectory.state_list,
-                             #                            margin=10))
-        self.rnd.draw_params.dynamic_obstacle.show_label = True
-        self.rnd.render()
         
-        utils_vis.draw_reference_path(self.rnd, np.array(self.clcs.reference_path()))
-        utils_vis.draw_state_list(self.rnd, self.ego_vehicle.prediction.trajectory.state_list[self.time_step:],
-                                  color=TUMcolor.TUMblue, linewidth=5)
-        other_veh_ids = [veh.obstacle_id for veh in self.sce.obstacles
-                         if veh.obstacle_id is not self.ego_vehicle.obstacle_id]
-        for v_id in other_veh_ids:
-            ci = self.compute(time_step=0, vehicle_id=v_id)
-            if ci > 0.0:
-                self.set_other_vehicles(v_id)
-                utils_vis.draw_state_list(self.rnd, self.other_vehicle.prediction.trajectory.state_list[self.time_step:],
-                             color=TUMcolor.TUMred,linewidth = 5)
+    def visualize(self, figsize: tuple = (25, 15)):
+        self._initialize_vis(figsize=figsize,
+                             plot_limit=utils_vis.
+                             plot_limits_from_state_list(self.time_step,
+                                                         self.ego_vehicle.prediction.trajectory.state_list,
+                                                         margin=50))
+        self.rnd.render()
+        if self.intersection is None:
+            utils_log.print_and_log_info(logger, "* No conflict area")
+            return 0
 
         plt.title(f"{self.measure_name} of {self.value} m")
-
-
-
-        if self.configuration.debug.draw_visualization:
-            if self.configuration.debug.save_plots:
-                utils_vis.save_fig(self.measure_name, self.configuration.general.path_output, self.time_step)
-            else:
-                plt.show()
         
+        for i in range(self.cte+1):            
+            x_i, y_i = self.ego_vehicle.occupancy_at_time(i).shape.shapely_object.exterior.xy
+            plt.plot(x_i, y_i, color='blue')
+            plt.fill(x_i, y_i, color='blue')
+            
+        for i in range(self.cto+1):            
+            x_i, y_i = self.other_vehicle.occupancy_at_time(i).shape.shapely_object.exterior.xy
+            plt.plot(x_i, y_i, color='orange')
+            plt.fill(x_i, y_i, color='orange')
+            
+        self.other_vehicle.occupancy_at_time(i).shape.shapely_object
+        if self.intersection.geom_type == "Polygon":
+            x_i, y_i = self.intersection.exterior.xy
+            plt.plot(x_i, y_i, color='red')
+            plt.fill(x_i, y_i, color='red')
+            centroid = self.intersection.centroid
+            x = centroid.x
+            y = centroid.y
+            print(self.v_y)
+            plt.arrow(x=x, y=y, dx=self.v_x, dy=self.v_y, head_width = 0.4, width = 0.1, ec ='red')
 
 
-
-
-
-
-
-
-
-
-
-
-"""
-    def visualize(self, figsize: tuple = (25, 15), plot_limit: Union[list, None] = None):
-        if plot_limit is None:
-            plot_limit = self.configuration.debug.plot_limits
-        self.rnd = MPRenderer(figsize=figsize, plot_limits=plot_limit)
-        self.rnd.draw_params.dynamic_obstacle.show_label = True
-        utils_vis.draw_sce_at_time_step(self.rnd, self.configuration, self.sce, self.time_step)
-        utils_vis.draw_state_list(self.rnd, self.ego_vehicle.prediction.trajectory.state_list[self.time_step:],
-                                  color=TUMcolor.TUMblue, linewidth=5)
-        other_veh_ids = [veh.obstacle_id for veh in self.sce.obstacles
-                         if veh.obstacle_id is not self.ego_vehicle.obstacle_id]
-        for v_id in other_veh_ids:
-            ci = self.compute(time_step=0, vehicle_id=v_id)
-            if ci > 0.0:
-                self.set_other_vehicles(v_id)
-                utils_vis.draw_state_list(self.rnd, self.other_vehicle.prediction.trajectory.state_list[self.time_step:],
-                             color=TUMcolor.TUMred,linewidth = 5)
-       
-        self.rnd.render()
-
-        plt.title(f"{self.measure_name} of {self.value} m")
-
+        
         if self.configuration.debug.draw_visualization:
             if self.configuration.debug.save_plots:
-                utils_vis.save_fig(self.measure_name, self.configuration.general.path_output, self.time_step)
+                utils_vis.save_fig(self.metric_name, self.configuration.general.path_output, self.time_step)
             else:
                 plt.show()
-
-"""
