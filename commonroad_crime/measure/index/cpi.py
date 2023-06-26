3eb4de8dbc749c0107524c4bb52d2c1f9fb56d26
yuanfei <yuanfei.lin@tum.de>
2023-06-26 14:26:49 +0200

    fix formatting

--- commonroad_crime/measure/index/cpi.py
+++ commonroad_crime/measure/index/cpi.py
@@ -38,11 +38,12 @@ class CPI(CriMeBase):
         super(CPI, self).__init__(config)
         self._a_lon_req_object = ALongReq(config)
         # TODO Add interface for different a_lon_min.
-        # We assume the MADR(aka. Maximum Avaliable Deceleration Rate)
+        # We assume the MADR (aka. Maximum Available Deceleration Rate)
         # is normally distributed.
         self.cpi_config = self.configuration.index.cpi
         self._madr_dist = norm(self.cpi_config.madr_mean,
                                self.cpi_config.madr_devi)
+        #  Survival function: gives the probability of obtaining a value larger than the given value
         self.tr_ub_prob = self._madr_dist.sf(self.cpi_config.madr_uppb)
         self.tr_lb_prob = self._madr_dist.cdf(self.cpi_config.madr_lowb)
         self.a_lon_req_list = []
@@ -59,7 +60,6 @@ class CPI(CriMeBase):
         self.time_step = time_step
         self.end_time_step = self.ego_vehicle.prediction.final_time_step
         self.value = 0
-        a_lon_req = 0
 
         for ts in range(self.time_step, self.end_time_step):
             try:
@@ -74,20 +74,20 @@ class CPI(CriMeBase):
             elif a_lon_req <= self.cpi_config.madr_lowb:
                 self.value += 1
             else:
-                # P(ALonReq>MADR) is equal to intergral of PDF
+                # P(ALonReq>MADR) is equal to integral of PDF
                 # from AlonReq to upperbound.
                 # -inf___lowerbound___ALonReq___upperbound___0___+inf
                 self.value += (self._madr_dist.sf(a_lon_req) - self.tr_ub_prob
                               ) / (1 - self.tr_lb_prob - self.tr_ub_prob)
 
-        # Nomalize the result with timespan.
+        # Normalize the result with timespan.
         self.value /= (self.end_time_step - self.time_step)
         utils_log.print_and_log_info(
             logger, f"*\t\t {self.measure_name} = {self.value}.")
         return self.value
 
     def visualize(self):
-        fig,ax = plt.subplots()
+        fig, ax = plt.subplots()
         x = np.linspace(self.cpi_config.madr_lowb, self.cpi_config.madr_uppb,100)
         # Plot the MADR distribution first.
         ax.plot(x, self._madr_dist.pdf(x),label='MADR')
@@ -96,14 +96,14 @@ class CPI(CriMeBase):
         cmap = cm.get_cmap("viridis", len(self.a_lon_req_list))
         colors = cmap(np.linspace(0, 1, len(self.a_lon_req_list)))
         # Plot datapoint at different time with different color
-        for dr,c in zip(self.a_lon_req_list,colors):
-            scatter=ax.scatter(dr,self._madr_dist.pdf(dr),color=c)
-        scatter.set_clim(self.time_step,self.end_time_step)
+        for dr, c in zip(self.a_lon_req_list, colors):
+            scatter = ax.scatter(dr, self._madr_dist.pdf(dr), color=c)
+        scatter.set_clim(self.time_step, self.end_time_step)
         # Draw colorbar
         cbar = plt.colorbar(scatter, ax=ax, orientation='horizontal')
         cbar.set_label('Time Step')
 
         plt.title("Deceleration Rate avoiding collision")
         plt.xlabel('Deceleration')
-        
+
         plt.show()
