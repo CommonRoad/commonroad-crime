3e51774041d33e98de22d2cea7586988245595c9
yuanfei <yuanfei.lin@tum.de>
2023-07-24 15:53:27 +0200

    update the sphinx_rtd_theme installation

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -5,14 +5,16 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('../../../commonroad_crime/'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 
 project = 'CommonRoad-CriMe'
 copyright = '2023, Technical University of Munich, Cyber-Physical Systems Group'
